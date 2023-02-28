03ac87d91bc4a471e6725490aa5c463e9482011e
lee <201250209@smail.nju.edu.cn>
2023-02-28 22:50:04 +0800

    javadoc for IdiomList and IronyList

--- src/main/java/uk/ac/wlv/sentistrength/IronyList.java
+++ src/main/java/uk/ac/wlv/sentistrength/IronyList.java
@@ -11,28 +11,53 @@ import java.io.*;
 import uk.ac.wlv.utilities.FileOps;
 import uk.ac.wlv.utilities.Sort;
 
-// Referenced classes of package uk.ac.wlv.sentistrength:
-//            ClassificationOptions
-
+/**
+ * 该类表示讽刺语列表，提供初始化列表，和判断术语是否讽刺语的方法
+ */
 public class IronyList
 {
 
+    /**
+     * 讽刺语字符串数组
+     */
     private String sgIronyTerm[];
+
+    /**
+     * 列表中讽刺语当前数量
+     */
     private int igIronyTermCount;
+
+    /**
+     * 列表中最大讽刺语容量
+     */
     private int igIronyTermMax;
 
+    /**
+     * 构造一个空的讽刺语列表
+     */
     public IronyList()
     {
         igIronyTermCount = 0;
         igIronyTermMax = 0;
     }
 
+    /**
+     * 判断输入的术语是否是讽刺语
+     * @param term 要检查的术语
+     * @return 是讽刺语返回true，反之返回false
+     */
     public boolean termIsIronic(String term)
     {
         int iIronyTermCount = Sort.i_FindStringPositionInSortedArray(term, sgIronyTerm, 1, igIronyTermCount);
         return iIronyTermCount >= 0;
     }
 
+    /**
+     * 使用输入的源文件路径、分类选项，初始化讽刺语列表
+     * @param sSourceFile 包含讽刺语的源文件路径
+     * @param options 一个ClassificationOptions类型的分类选项
+     * @return 初始化成功则返回true，反之则返回false
+     */
     public boolean initialise(String sSourceFile, ClassificationOptions options)
     {
         if(igIronyTermCount > 0)
