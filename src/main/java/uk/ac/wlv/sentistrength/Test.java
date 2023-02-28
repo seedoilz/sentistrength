65a681376dcf61a967e8211e4d9996091c27fe47
zhangruohao <201250203@smail.nju.edu.cn>
2023-02-28 13:33:48 +0800

    Javadoc for Test

--- src/main/java/uk/ac/wlv/sentistrength/Test.java
+++ src/main/java/uk/ac/wlv/sentistrength/Test.java
@@ -10,13 +10,29 @@ import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 
+/**
+ * Test类，test字符串是否为纯ASCII编码
+ * @author ruohao.zhang
+ **/
 public class Test
 {
 
+    /**
+     * Test构造器
+     * @author ruohao.zhang
+     */
     public Test()
     {
     }
 
+    /**
+     * Test类的main函数，对字符串进行ASCII编码的测试
+     * asciiEncoder.canEncode()方法测试字符串是否为纯ASCII编码
+     * 如果字符串不是纯ASCII编码，程序将打印出所有非ASCII字符的值。
+     * 最后进行URL编码，并将其打印到控制台上。
+     * @param args
+     * @author ruohao.zhang
+     */
     public static void main(String args[])
     {
         CharsetEncoder asciiEncoder = Charset.forName("US-ASCII").newEncoder();
