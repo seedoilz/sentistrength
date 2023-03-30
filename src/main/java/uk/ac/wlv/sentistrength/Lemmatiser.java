da833e33b320a6f3b5a2626ff2e22fca31458802
DaiXuezheng <201250191@smail.nju.edu.cn>
2023-03-30 14:57:36 +0800

    Lemmatiser commit2

--- src/main/java/uk/ac/wlv/sentistrength/Lemmatiser.java
+++ src/main/java/uk/ac/wlv/sentistrength/Lemmatiser.java
@@ -15,21 +15,19 @@ import uk.ac.wlv.utilities.Sort;
  * 将单词换原为其原形的工具类
  * @author DaiXuezheng
  */
-public class Lemmatiser
-{
+public class Lemmatiser {
 
     /** 单词数组 **/
-    private String sgWord[];
+    private String[] sgWord;
     /** 单词对应的原形的数组 **/
-    private String sgLemma[];
+    private String[] sgLemma;
     /** 存储数组中最后一个单词的位置 **/
     private int igWordLast;
 
     /**
      * 构造函数
      */
-    public Lemmatiser()
-    {
+    public Lemmatiser() {
         igWordLast = -1;
     }
 
@@ -40,67 +38,61 @@ public class Lemmatiser
      * @return 成功读取文件返回 true，否则返回false
      * @author DaiXuezheng
      */
-    public boolean initialise(String sFileName, boolean bForceUTF8)
-    {
+    public boolean initialise(String sFileName, boolean bForceUTF8) {
         int iLinesInFile = 0;
-        if(sFileName.equals(""))
-        {
-            System.out.println("No lemma file specified!");
+        if (sFileName.equals("")) {
+            System.err.println("No lemma file specified!");
             return false;
         }
         File f = new File(sFileName);
-        if(!f.exists())
-        {
-            System.out.println((new StringBuilder("Could not find lemma file: ")).append(sFileName).toString());
+        if (!f.exists()) {
+            System.err.println((new StringBuilder("Could not find lemma file: ")).append(sFileName).toString());
             return false;
         }
         iLinesInFile = FileOps.i_CountLinesInTextFile(sFileName);
-        if(iLinesInFile < 2)
-        {
-            System.out.println((new StringBuilder("Less than 2 lines in sentiment file: ")).append(sFileName).toString());
+        if (iLinesInFile < 2) {
+            System.err.println((new StringBuilder("Less than 2 lines in sentiment file: ")).append(sFileName).toString());
             return false;
         }
         sgWord = new String[iLinesInFile + 1];
         sgLemma = new String[iLinesInFile + 1];
         igWordLast = -1;
-        try
-        {
+        try {
             BufferedReader rReader;
-            if(bForceUTF8)
+            if (bForceUTF8) {
                 rReader = new BufferedReader(new InputStreamReader(new FileInputStream(sFileName), "UTF8"));
-            else
+            } else {
                 rReader = new BufferedReader(new FileReader(sFileName));
+            }
             String sLine;
-            while((sLine = rReader.readLine()) != null) 
-                if(sLine != "")
-                {
+            while ((sLine = rReader.readLine()) != null) {
+                if (!sLine.equals("")) {
                     int iFirstTabLocation = sLine.indexOf("\t");
-                    if(iFirstTabLocation >= 0)
-                    {
+                    if (iFirstTabLocation >= 0) {
                         int iSecondTabLocation = sLine.indexOf("\t", iFirstTabLocation + 1);
                         sgWord[++igWordLast] = sLine.substring(0, iFirstTabLocation);
-                        if(iSecondTabLocation > 0)
+                        if (iSecondTabLocation > 0) {
                             sgLemma[igWordLast] = sLine.substring(iFirstTabLocation + 1, iSecondTabLocation);
-                        else
+                        } else {
                             sgLemma[igWordLast] = sLine.substring(iFirstTabLocation + 1);
-                        if(sgWord[igWordLast].indexOf(" ") >= 0)
-                            sgWord[igWordLast] = sgWord[igWordLast].trim();
-                        if(sgLemma[igWordLast].indexOf(" ") >= 0)
-                            sgLemma[igWordLast] = sgLemma[igWordLast].trim();
+                            if (sgWord[igWordLast].indexOf(" ") >= 0) {
+                                sgWord[igWordLast] = sgWord[igWordLast].trim();
+                            }
+                            if (sgLemma[igWordLast].indexOf(" ") >= 0) {
+                                sgLemma[igWordLast] = sgLemma[igWordLast].trim();
+                            }
+                        }
                     }
                 }
-            rReader.close();
-            Sort.quickSortStringsWithStrings(sgWord, sgLemma, 0, igWordLast);
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println((new StringBuilder("Couldn't find lemma file: ")).append(sFileName).toString());
+                rReader.close();
+                Sort.quickSortStringsWithStrings(sgWord, sgLemma, 0, igWordLast);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println((new StringBuilder("Couldn't find lemma file: ")).append(sFileName).toString());
             e.printStackTrace();
             return false;
-        }
-        catch(IOException e)
-        {
-            System.out.println((new StringBuilder("Found lemma file but couldn't read from it: ")).append(sFileName).toString());
+        } catch (IOException e) {
+            System.err.println((new StringBuilder("Found lemma file but couldn't read from it: ")).append(sFileName).toString());
             e.printStackTrace();
             return false;
         }
@@ -114,12 +106,12 @@ public class Lemmatiser
      * @return 单词对应的原形
      * @author DaiXuezheng
      */
-    public String lemmatise(String sWord)
-    {
+    public String lemmatise(String sWord) {
         int iLemmaID = Sort.i_FindStringPositionInSortedArray(sWord, sgWord, 0, igWordLast);
-        if(iLemmaID >= 0)
+        if (iLemmaID >= 0) {
             return sgLemma[iLemmaID];
-        else
+        } else {
             return sWord;
+        }
     }
 }
