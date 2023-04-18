5fde7410a228a7cf4492205159547ccbdcf5fa51
yuhaofeng <201250210@smail.nju.edu.cn>
2023-04-18 15:59:24 +0800

    修改脚本4

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 verson="1.0.2"
-url="https://git.nju.edu.cn/api/v4/projects/8331/packages/generic/my_package/"+$verson+"/SentiStrength.jar"
-dataInfo='{ "name": "release", "tag_name": "v1.0.2", "description": "2.0", "assets": { "links": [{ "name": "hoge", "url": "https://git.nju.edu.cn/api/v4/projects/8331/packages/generic/my_package/'+$verson + '/SentiStrength.jar", "direct_asset_path": "/binaries/arm64", "link_type":"other" }] } }'
+url="https://git.nju.edu.cn/api/v4/projects/8331/packages/generic/my_package/"${verson}"/SentiStrength.jar"
+dataInfo='{ "name": "release", "tag_name": "v1.0.2", "description": "2.0", "assets": { "links": [{ "name": "hoge", "url": "https://git.nju.edu.cn/api/v4/projects/8331/packages/generic/my_package/'${verson}'/SentiStrength.jar", "direct_asset_path": "/binaries/arm64", "link_type":"other" }] } }'
 function release()
 {
   cd /var/lib/jenkins/workspace/test
