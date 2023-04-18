de6f8633a024fbaa8d570ca38254c3e5a277b651
yuhaofeng <201250210@smail.nju.edu.cn>
2023-04-18 16:23:45 +0800

    修改脚本6

--- start.sh
+++ start.sh
@@ -7,9 +7,10 @@ function release()
   cd /var/lib/jenkins/workspace/test
   curl --header "PRIVATE-TOKEN: glpat-vHkkwBeo8wbELj6yW3sr" \
        --upload-file target/SentiStrength.jar \
-       ${url}
+      "https://git.nju.edu.cn/api/v4/projects/8331/packages/generic/my_package/"${version}"/SentiStrength.jar"
+
   curl --header 'Content-Type: application/json' --header "PRIVATE-TOKEN: glpat-vHkkwBeo8wbELj6yW3sr" \
-       --data ${dataInfo} \
+       --data '{ "name": "release", "tag_name": "'${version}'", "description": "2.0", "assets": { "links": [{ "name": "sentiStrength", "url": "https://git.nju.edu.cn/api/v4/projects/8331/packages/generic/my_package/'${version}'/SentiStrength.jar", "direct_asset_path": "/binaries/arm64", "link_type":"other" }] } }' \
        --request POST "https://git.nju.edu.cn/api/v4/projects/8331/releases"
 
 }
